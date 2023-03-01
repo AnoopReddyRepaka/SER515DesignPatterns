3f08e520507dd2261c69be48a73fa3c0feae0a6a
chenxi <chenxix2@asu.edu>
2023-03-01 16:43:01 -0700

    Add factory files

---
+++ source/UAV Deployment.java
@@ -0,0 +1,21 @@
+public class UAV Deployment {
+
+	private int Drone[];
+
+	public void sendNowUAV() {
+
+	}
+
+	public void setLocation() {
+
+	}
+
+	public void setNumberofDrone() {
+
+	}
+
+	public int sendFireFightingUAV() {
+		return 0;
+	}
+
+}
---
+++ source/UAVDeploymentFactory.java
@@ -0,0 +1,13 @@
+public class UAVDeploymentFactory extends UAV Deployment {
+
+	private UAV Deployment uAV Deployment;
+
+	public void DeployNavFleet() {
+
+	}
+
+	public void DeployFireFightFleet() {
+
+	}
+
+}
